dec29228c002b3e9b6b5a6f9d75f3a71d83aa52c
Oliver Jang <jangoliver@gmail.com>
2019-11-20 10:33:49 -0800

    Fixing my shit

---
+++ src/hal/shapes.h
@@ -0,0 +1,68 @@
+#ifndef SHAPES_H
+#define SHAPES_H
+
+#define PIXEL_SIZE 4
+#define MAX_BULLETS 3
+#define MAX_ENEMIES 20
+
+enum ObjType{Enemy, Ship, Bullet}; 
+
+typedef struct {
+	int16_t x;
+	int16_t y; 
+} Point;
+
+typedef struct {
+	Point p;
+	uint32_t colour;
+} Pixel;
+
+typedef struct {
+	Point start;
+	Point end;
+} Line;
+
+typedef struct {
+	Pixel pixels[81];
+	uint8_t pixelNum;
+} Shape;
+
+typedef struct {
+	Point origin;//center of object
+	enum ObjType type;
+	uint8_t currentPath;
+	uint8_t pathPos;
+	Point start;
+} Object;
+
+
+typedef struct {
+	Line lines[10];
+	uint32_t numLines;
+} FontCharacter;
+
+uint32_t charSize;
+FontCharacter characters[26];
+void shapes_init(void);
+FontCharacter* getCharacter(uint8_t c);
+
+
+
+// Shape ship;
+Object ship;
+Object enemy;
+Object enemyArr[MAX_ENEMIES];
+uint8_t curEnemyArr[MAX_ENEMIES];
+uint8_t curEnemy;
+void addEnemy(uint8_t index);
+void delEnemy(uint8_t index);
+Point addPoint(Point p1, Point p2);
+Point subtractPoint(Point p1, Point p2);
+
+Shape objectShapes[3];
+Point objectSize[3];
+
+Point enemyPath[5][128];
+Object bulletArr[MAX_BULLETS];
+
+#endif
